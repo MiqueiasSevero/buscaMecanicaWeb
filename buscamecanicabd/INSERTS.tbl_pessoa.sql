5a35b741827fd16acd87c74779293d3880fd68e4
MiqueiasSevero <43190592+MiqueiasSevero@users.noreply.github.com>
2019-12-05 21:50:01 -0300

    mudança no insert

--- buscamecanicabd/INSERTS.tbl_pessoa.sql
+++ buscamecanicabd/INSERTS.tbl_pessoa.sql
@@ -12,8 +12,10 @@ INSERT INTO `buscamecanica`.`tbl_pessoa`
 `cidade`,
 `cep`,
 `uf`,
+ `celular`,
+ `telefone`,
 `pais`,
 `numero`,
 `complemento`)
 VALUES
-(NULL ,'JANAINA', 'SILVA' ,'45646105880','jana@gmail.com', '123','O','1','MARIA DALVA AGUIAR', 'JARDIM VITÓRIA}',' TABOÃO DA SERRA','06784426','SP',NULL, '65','NENHUM');
+(NULL ,'JANAINA', 'SILVA' ,'45646105880','jana@gmail.com', '123','O','1','MARIA DALVA AGUIAR', 'JARDIM VITÓRIA}',' TABOÃO DA SERRA','06784426','SP','11969001229','1146852642',NULL, '65','NENHUM');
